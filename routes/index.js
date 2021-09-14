4f464c80b39308a3330cd74a6e6ae265eefb195a
mahmoud-64 <mahmoudyoussef070@gmail.com>
2021-09-14 11:00:30 +0200

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(process.env);
-  // res.render('index', { title: 'Express' });
+  // console.log(process.env);
+environment = process.env
+  return res.json( { environment: environment });
 });
 
 module.exports = router;
